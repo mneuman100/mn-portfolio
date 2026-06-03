ff2022c489c8efa45cfdf2769e6c1394cad12845
Alex Jones <iceclimber999@gmail.com>
2026-06-02 22:19:12 -0400

    Add final case study

--- src/app/pages/ai-tools-case-study-page.ts
+++ src/app/pages/ai-tools-case-study-page.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { SkillColor, SkillModel } from '../models/skill-model';
+import { CaseStudyHeroComponent } from "../components/case-study-hero-component";
 
 @Component({
   selector: 'app-ai-tools-case-study-page',
-  imports: [],
+  imports: [CaseStudyHeroComponent],
   templateUrl: './ai-tools-case-study-page.html',
   styleUrl: './ai-tools-case-study-page.scss',
 })
 export class AiToolsCaseStudyPage {
-
+  skills: SkillModel[] = [
+    { skill: 'Usability Evaluation',           color: SkillColor.Blue   },
+    { skill: 'Heuristic Evaluation',           color: SkillColor.Blue   },
+    { skill: 'Qualitative Research',           color: SkillColor.Blue   },
+    { skill: 'Structured Testing',             color: SkillColor.Blue   },
+    { skill: 'Stakeholder Interviews',         color: SkillColor.Green  },
+    { skill: 'Risk Assessment',                color: SkillColor.Purple },
+    { skill: 'Data Analysis',                  color: SkillColor.Blue   },
+    { skill: 'Insight Synthesis',              color: SkillColor.Green  },
+    { skill: 'Research Reporting',             color: SkillColor.Purple },
+    { skill: 'Cross-functional Collaboration', color: SkillColor.Green  },
+  ];
 }
